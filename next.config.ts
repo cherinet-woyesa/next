26d223c9d8a0d04c3843ba7640ce0ee28f5fc3e2
cherinet-woyesa <woyesabizunesh@gmail.com>
2025-05-29 14:12:43 +0300

    some

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), {
+      "swiper/css": "commonjs swiper/css",
+      "swiper/css/effect-coverflow": "commonjs swiper/css/effect-coverflow",
+      "swiper/css/pagination": "commonjs swiper/css/pagination"
+    }];
+    return config;
+  }
 };
 
 export default nextConfig;
