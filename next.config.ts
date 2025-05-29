443d460d5c1395144a5a6729720e23555808cb4e
cherinet-woyesa <woyesabizunesh@gmail.com>
2025-05-29 14:35:45 +0300

    fix: Configure Swiper CSS modules in webpack

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,23 @@ const nextConfig: NextConfig = {
       "swiper/css/effect-coverflow": "commonjs swiper/css/effect-coverflow",
       "swiper/css/pagination": "commonjs swiper/css/pagination"
     }];
+    
+    // Add CSS modules configuration
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            importLoaders: 1,
+            localIdentName: '[name]__[local]__[hash:base64:5]'
+          }
+        }
+      ]
+    });
+    
     return config;
   }
 };
