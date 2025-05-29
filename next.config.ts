bc2b69288ab6464b08eafe75792102d2b422d10e
cherinet-woyesa <woyesabizunesh@gmail.com>
2025-05-29 14:31:49 +0300

    refactor: Remove Swiper and replace with simpler grid layout

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
-    // Add Swiper CSS as external
-    config.externals = [...(config.externals || []), {
-      "swiper/css": "commonjs swiper/css",
-      "swiper/css/effect-coverflow": "commonjs swiper/css/effect-coverflow",
-      "swiper/css/pagination": "commonjs swiper/css/pagination"
-    }];
-
-    // Add CSS module rules for Swiper
-    config.module.rules.push({
-      test: /swiper\/.+\.css$/,
-      use: ['style-loader', 'css-loader']
-    });
-
     return config;
   }
 };
